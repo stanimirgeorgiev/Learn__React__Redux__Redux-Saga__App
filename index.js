a14c3bfee423f79bbf0dec629df2686dd986e3d3
Stanimir Georgiev <az.stanimir@gmail.com>
2019-02-12 23:27:28 +0200

    cleared all but react and redux

--- index.js
+++ index.js
@@ -3,30 +3,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { fork } from 'redux-saga/effects'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import rootSagas from './app/sagas/root-saga';
 import appReducer from './app/reducers/app';
 
 import UsersTasks from './app/views/users-tasks-container';
 import './css/styles.css';
 
 const reducers = appReducer;
-const sagaMiddleware = createSagaMiddleware(rootSagas);
 const store = createStore(
-  reducers,
-  applyMiddleware(sagaMiddleware)
+  reducers
 );
-
-const createRootSaga = function* (sagas) {
-  for (let i = 0; i < sagas.length; i++) {
-    const saga = sagas[i];
-    yield fork(saga);
-  }
-}
 
 const appDomId = 'root';
 const rootContainer = document.getElementById(appDomId);
@@ -38,7 +26,5 @@ const reactComponent = (
     </React.Fragment>
   </Provider>
 );
-
-sagaMiddleware.run(createRootSaga, rootSagas);
 
 render(reactComponent, rootContainer);
